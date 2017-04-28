86cea31ceb2ae19cc8267a7b98d15f1212b6adf1
Elad Keyshawn <elad.keyshawn@gmail.com>
2017-04-28 23:55:36 +0300

    fixed scroll anim

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
+// $(document).on('click', 'a', function(event){
+//     event.preventDefault();
+    
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+// });
 
+$('.scroll_link').click(function(){
+    
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
